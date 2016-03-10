aee06f6391ac977ec13a74a7fd986aba8ea08176
benrules2 <bceagan@gmail.com>
2016-03-09 21:06:11 -0500

    Log all weather data

--- data_display.py
+++ data_display.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def log_to_file(filename, current_min, current_max, record_min, record_max, datetime_info):
+def log_to_file(filename, current_min, current_max, avg_min, avg_max, record_min, record_max, datetime_info):
     f = open(filename,'a+')
-    f.write("{0},{1},{2},{3},{4}\n".format(current_min, current_max, record_min, record_max, str(datetime_info.time())))
+    f.write("{0},{1},{2},{3},{4}\n".format(current_min, current_max, avg_min, avg_max, record_min, record_max, str(datetime_info.time())))
 
